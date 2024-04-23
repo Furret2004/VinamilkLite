4fa073bec50c27ccdb7a77f7d1619d8c0562758a
Furret2004 <tranngocchaulong2362004@gmail.com>
2024-04-23 20:57:31 +0700

    no message

--- client/src/pages/common/LoginPage.jsx
+++ client/src/pages/common/LoginPage.jsx
@@ -3,19 +3,37 @@ import { useAuth } from '../../hooks';
 import { Loading } from '../../components/common';
 import { useNavigate } from 'react-router-dom';
 
+
 function LoginPage() {
   const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { profile, login, refresh } = useAuth();
   const navigate = useNavigate();
+  const [emailCheck, setEmailCheck] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState(''); 
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    if (email.trim() === '') {
+      setEmailCheck('Email la bat buoc');
+      return;
+    }
+    if (!emailRegex.test(email)) {
+      setEmailCheck('Email khong hop le');
+      return;
+    }
+    setEmailCheck('');
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+    if (password === '') {
+      setPasswordCheck('Mat khau la bat buoc');
+      return;
+    }
+    setPasswordCheck('');
   };
 
   const handleLogin = async (event) => {
@@ -60,7 +78,8 @@ function LoginPage() {
             value={email} 
             type="email" 
             placeholder="email"
-            required pattern="[A-Za-z0-9]+@[A-Za-z0-9]+"/><br></br>
+          />{emailCheck && <p className='text-red-600'>{emailCheck}</p>}
+          <br></br>
           <p className='italic text-blue-400'>Mật khẩu</p>
           <input
             className='border border-blue-400 border-solid rounded-lg'
@@ -68,7 +87,7 @@ function LoginPage() {
             value={password}
             type="password"
             placeholder="password"
-          />
+          />{passwordCheck && <p className='text-red-600'>{passwordCheck}</p>}
           <br></br>
           <button onClick={handleLogin} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
             Đăng nhập
